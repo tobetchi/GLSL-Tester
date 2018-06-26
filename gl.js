ff7d04916630cceb19bc8e0080e4030c1a6d7180
tobetchi <tobetchi@gmail.com>
2018-06-26 19:58:26 +0900

    Change canvas size to window size

--- gl.js
+++ gl.js
@@ -1,6 +1,4 @@
-var gl, prg;
-var width = 512;
-var height = 512;
+var gl, prg, canvas;
 
 window.onload = function(){
     init();
@@ -9,8 +7,9 @@ window.onload = function(){
 function init() {
     if(!canvas) canvas = document.getElementById('canvas');
     if(!gl) gl = canvas.getContext('webgl');
-    canvas.width = width;
-    canvas.height = height;
+    document.body.style.margin = 0;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     prg = gl.createProgram();
     var vSource = 'attribute vec3 p;void main(){gl_Position=vec4(p,1.);}';
     readFile(document.getElementById('fs').getAttribute('data-src'),
@@ -55,13 +54,13 @@ function render() {
     gl.enableVertexAttribArray(a);
     gl.vertexAttribPointer(a, 3, gl.FLOAT, false, 0, 0);
     gl.clearColor(0, 0, 0, 1);
-    gl.viewport(0, 0, width, height);
+    gl.viewport(0, 0, canvas.width, canvas.height);
     z = new Date().getTime();
     (function(){
         d = (new Date().getTime() - z) * 0.001;
         gl.clear(gl.COLOR_BUFFER_BIT);
         gl.uniform1f(uni.time, d);
-        gl.uniform2fv(uni.resolution, [width, height]);
+        gl.uniform2fv(uni.resolution, [canvas.width, canvas.height]);
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
         gl.flush();
         requestAnimationFrame(arguments.callee);
